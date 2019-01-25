be0d38e2bcfad1e7aa181005e919afbeb6a029cd
Geofrey <g.s.mushibi@students.uu.nl>
2019-01-25 15:54:24 +0100

    Final code for 20th century data + CSV file containing totals

--- 20th_century.R
+++ 20th_century.R
@@ -17,18 +17,9 @@ century$pneumonia <- cbind(century$`Pneumonia and influenza (deaths per 100,000)
 century$suicide <- cbind(century$`Suicide (deaths per 100,000)`, na.rm = TRUE)
 
 #deleting extra columns in dataset 
-data = century %>% select(accidents_murder_and_war, cancer, cadiovascular_problems, other_diseases, other_respiratory_diseases, pneumonia, suicide)
+america_data = century %>% select(accidents_murder_and_war, cancer, cadiovascular_problems, other_diseases, other_respiratory_diseases, pneumonia, suicide)
 
-#adding columns
-#colSums(data.num)
-library(data.table)
-new_data <- as.data.frame.table(read.csv(data.choose(), header = T))
-new_data[total = sum(cancer)]
-
-
-total_cancer <- data %>% 
-  #group_by(cancer) %>%
-  summarise(total_cancer = sum(cancer))
+#finding the total sums of each column 
 sum(as.numeric(data$accidents_murder_and_war), na.rm = TRUE)
 sum(as.numeric(data$cancer), na.rm = TRUE)
 sum(as.numeric(data$cadiovascular_problems), na.rm = TRUE)
@@ -37,16 +28,23 @@ sum(as.numeric(data$other_respiratory_diseases), na.rm = TRUE)
 sum(as.numeric(data$pneumonia), na.rm = TRUE)
 sum(as.numeric(data$suicide), na.rm = TRUE)
 
-ggplot(data=americaperc)+
-  geom_bar(stat = 'identity', mapping=aes(x=Cause_of_death, y = frequency, color = Cause_of_death, fill = Cause_of_death)) +
-  theme(axis.text.x = element_text(angle= 70, hjust = 1)) +
-  labs(title = "Most Common Causes of Death in the 20th Century", x = "Cause of Death", y = "Percentage of Deaths", color = "Cause of Death") +
-  scale_y_continuous(labels= scales::percent_format()) +
-  theme(legend.position = "none")
+#after finding the totals for each disease category, the data was manually converted into a csv file using a excel
+#The new csv file is now called "america_dataset"
 
-americaperc <- america %>%
+#plotting the graph 
+americaperc <- america_dataset %>%
   mutate(
     total_deaths = sum(Total),
     frequency = Total/total_deaths,
-    Cause_of_death = ifelse(str_detect(Cause_of_death, 'cadio'), 'cardiovascular_diseases', Cause_of_death)
+    Cause_of_death = ifelse(str_detect(Cause_of_death, 'cadio'), 'cardiovascular_diseases', Cause_of_death),
+    Cause_of_death = str_replace_all(Cause_of_death, '_', ' ')
   )
+
+ggplot(data=americaperc)+
+  geom_bar(stat = 'identity', mapping=aes(x=Cause_of_death, y = frequency, color = Cause_of_death, fill = Cause_of_death)) +
+  labs(title = "Most Common Causes of Death in the 20th Century", x = "Cause of Death", y = "Percentage of Deaths", color = "Cause of Death") +
+  scale_y_continuous(labels= scales::percent_format()) +
+  theme(legend.position = "none") +
+  coord_flip()
+
+
