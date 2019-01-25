581258210daba8d374e1e9f3a92a9ea23a3f1f29
Tom <scassellati98@gmail.com>
2019-01-25 16:04:28 +0100

    The ABSOLUTE FINAL code for the time period data

--- Modern_R_Script.R
+++ Modern_R_Script.R
@@ -20,8 +20,8 @@ Counts2$percent <- (Counts2$n / sum(Counts2$n))
 Counts2$time_period <- c("Time_Period")
 Counts2$time_period <- "Enlightenment/Industrial"
 
-#Bind the Counts1 and Counts2 frames
-total_eras<- rbind(Counts1, Counts2)
+#Bind the Counts1 and Counts2 frames and replace '_' in variable names to spaces
+total_eras<- rbind(Counts1, Counts2) %>% mutate(deathCause_label= str_replace_all(deathCause_label,'_',' '))
 
 #Count the number of occurences for all the causes and make it into a data frame
 Counts <- count(total_eras, deathCause_label)
@@ -32,13 +32,14 @@ total_eras$percent <- Counts$n / sum(Counts$n)
 ggplot()+
   #stat= 'identity' allows for the y variable mapped in the aesthetic to be computed
   geom_bar(data=total_eras, mapping=aes(x= deathCause_label, y= percent, color= time_period, fill= time_period), stat= 'identity', position= 'dodge')+
-  #Angle the labels to 70 degrees so that they become legible
-  theme(axis.text.x.bottom = element_text(angle=70, hjust = 1))+
+  #Increase size of the x and y variable labels as well
+  theme(axis.text.x.bottom = element_text(size=10))+
+  theme(axis.text.y.left = element_text(size=12))+
   #Make a title as well as a new x and y axis label
-  labs(title = "Comparing Deaths in the Modern Era and the Enlightenment/Industrial Era", x= "Cause of Death", y= "Proportion of Total Deaths", color= "Time Period", fill= "Time Period")+
+  labs(title = "Comparing Deaths in the Modern Era and the Enlightenment/Industrial Era", x= "Cause of Death", y= "Percentage of Total Deaths", color= "Time Period", fill= "Time Period")+
   #Turn the y variable into percentage format
-  scale_y_continuous(labels = scales::percent_format())
-  
+  scale_y_continuous(labels = scales::percent_format())+
+  #Flip the graph horizontally for legibility
+  coord_flip()
 
-
-
+Modern_Industrial <- rbind(modern,industrial)
